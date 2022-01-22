ff5d8cf24aa4ae893b5475628e1b0be2fe2658db
Becasuse-hung <4a33a006@stust.edu.tw>
2022-01-23 02:21:25 +0800

    basic composition api

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import componentA from './components/componentA.vue'
+</script>
+
+<template>
+ <componentA></componentA>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/componentA.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref, reactive, onMounted, watch } from 'vue'
+const testf = ref({
+    idx:0,
+    name:'10'
+})
+const txt = ref('123')
+const person = reactive({
+    name: 'Bob'
+})
+const person2 = ref({
+    name:'Tommy'
+})
+console.log('test', testf.value)
+console.log('log', person2.value)
+function getWorld(){
+    console.log('hello World')
+}
+onMounted(()=>{
+    getWorld()
+})
+
+console.log(person)
+
+watch(testf.value, (nval)=>{
+console.log('log', nval)
+})
+
+setTimeout(()=>{
+    testf.value.idx = 1
+},1000)
+</script>
+
+<template>
+<div>
+ <h2>v3 hello world</h2>
+ <input type="text" v-model="txt" />
+ {{ txt }}
+ <p>reative - {{ person.name }}</p>
+ <p>ref - {{ person2.name  }}</p>
+ <button @click="getWorld">enter me</button>
+ </div>
+</template>
+
+<style>
+
+</style>
